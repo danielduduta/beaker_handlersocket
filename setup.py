e9d9fc0642a1a5fa1f2d06e7f3052443fc175eca
Daniel Gavrila <danielduduta@gmail.com>
2015-08-29 16:33:57 +0300

    Initial setup

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import multiprocessing
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'beaker_handlersocket',
+    version = '0.1',
+    description = 'Beaker backend to write sessions to MySQL using HandlerSocket.',
+    long_description = '\n' + open('README.rst').read(),
+    author='Daniel Gavrila',
+    author_email = 'danielduduta@gmail.com',
+    keywords = 'handlersocket beaker session',
+    license = 'New BSD License',
+    url = 'https://github.com/danielduduta/beaker_handlersocket/',
+    tests_require = ['nose', 'webtest'],
+    test_suite='nose.collector',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe = True,
+    entry_points="""
+    [beaker.backends]
+    handlersocket = beaker_handlersocket:HandlerSocketMySQLNamespaceManager    
+    """,
+    install_requires=[
+        'python-handler-socket==0.2.4',
+        'beaker>=1.6.4'
+    ]
+)
---
+++ src/beaker_handlersocket/__init__.py
@@ -0,0 +1,115 @@
+import logging
+import cPickle as pickle
+
+from datetime import datetime
+from beaker.container import NamespaceManager, Container
+from beaker.exceptions import InvalidCacheBackendError, MissingCacheParameter
+from beaker.synchronization import null_synchronizer
+from beaker.util import verify_directory, SyncDict
+from pyhs import Manager
+
+
+
+log = logging.getLogger(__name__)
+
+
+def parse_servers_for_hs(servers):
+    entries = []
+    for server in servers:
+        host, port = server.split(':')
+        entries.append(('inet', host, int(port)))
+
+    return entries
+
+
+class HandlerSocketMySQLNamespaceManager(NamespaceManager):
+    
+    clients = SyncDict()
+    _pickle = True
+
+    def __init__(self, namespace, read_servers=None, write_servers=None, 
+            database=None, table=None, index=None, data_dir=None, skip_pickle=False, **kw):
+        
+        self.database = database
+        self.table = table
+        self.index = index
+
+        NamespaceManager.__init__(self, namespace)
+
+        data_key = "hs:%s" % (database)
+
+        def _initiate_connections(read_servers, write_servers):
+            read_servers = parse_servers_for_hs(read_servers)
+            write_servers = parse_servers_for_hs(write_servers)
+            hs = Manager(read_servers, write_servers)
+            return hs
+
+        self.hs = HandlerSocketMySQLNamespaceManager.clients.get(data_key,
+                    _initiate_connections, read_servers, write_servers)
+
+    
+    def _format_key(self, key):
+        key = self.namespace
+        return key
+
+
+    def get_creation_lock(self, key):
+        return null_synchronizer
+
+    
+    def do_remove(self):
+        raise NotImplementedError('Method not supported')
+
+
+    def __getitem__(self, key):
+        key = self._format_key(key)
+        log.debug("Searching for key {}".format(key))
+        result = self.hs.find(self.database, self.table, '=', ['id', 'data'], [key], self.index, 1)
+        data = result
+        if result:
+            data = pickle.loads(result[0][1][1])
+        return data
+
+    
+    def __contains__(self, key):
+        key = self._format_key(key)
+        result = self.__getitem__(key)
+        return result
+
+    
+    def has_key(self, key):
+        key = self._format_key(key)
+        return key in self
+
+    
+    def set_value(self, key, value, expiretime=None):
+        value = pickle.dumps(value)
+        key = self._format_key(key)
+        created = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        ekey = self.has_key(key)
+        if not ekey:
+            self.hs.insert(self.database, self.table, [('id', key),('data',value), ('created', created)])
+        else:
+            self.hs.update(self.database, self.table, '=', ['id', 'data'], [key], [key, value])
+
+    
+    def __setitem__(self, key, value):
+        key = self._format_key(key)
+        self.set_value(key, value)
+
+    
+    def __delitem__(self, key):
+        key = self._format_key(key)
+        self.hs.delete(self.database, self.table, '=', ['id'], [key])
+
+    
+    def keys(self):
+        raise NotImplementedError('Method not supported')
+
+
+class HandlerSocketMySQLContainer(Container):
+
+    namespace_class = HandlerSocketMySQLNamespaceManager
+
+
